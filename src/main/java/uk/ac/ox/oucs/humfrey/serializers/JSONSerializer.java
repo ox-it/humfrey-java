ce91c8ed1c9a8510b6b88d4b4163e32771478540
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-07-14 17:13:52 +0100

    Added ResultSet serialization to JSONSerializer.

--- src/main/java/uk/ac/ox/oucs/humfrey/serializers/JSONSerializer.java
+++ src/main/java/uk/ac/ox/oucs/humfrey/serializers/JSONSerializer.java
@@ -2,14 +2,21 @@ package uk.ac.ox.oucs.humfrey.serializers;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import uk.ac.ox.oucs.humfrey.Query;
+import org.apache.commons.lang.StringEscapeUtils;
 
+import uk.ac.ox.oucs.humfrey.Query;
+import uk.ac.ox.oucs.humfrey.resources.VelocityResource;
+
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
@@ -43,6 +50,7 @@ class JSONSerializer extends AbstractSerializer {
 	public void serializeModel(Model model, Model fullModel, Query query,
 			HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		resp.setContentType(getContentType());
 		
 		Writer writer = resp.getWriter();
 		
@@ -93,6 +101,67 @@ class JSONSerializer extends AbstractSerializer {
 		}
 		
 		writer.write("}");
+	}
+	
+	@Override
+	public void serializeResultSet(ResultSet resultset, Query query, HttpServletRequest req, HttpServletResponse resp)  throws IOException {
+		boolean notFirst = false;
+		resp.setContentType(getContentType());
+		List<String> bindings = resultset.getResultVars();
+		
+		Writer writer = resp.getWriter();
+
+		if (asJavaScript && req.getParameter("callback") != null)
+			writer.write(req.getParameter("callback")+"(");
+		writer.write("{bindings: [");
+		for (String binding : bindings) {
+			if (notFirst)
+				writer.write(", ");
+			else
+				notFirst = true;
+			writer.write("\"" + StringEscapeUtils.escapeJavaScript(binding) + "\"");
+		}
+		writer.write("], results: [");
+		while (resultset.hasNext()) {
+			writer.write("{");
+			QuerySolution soln = resultset.next();
+			notFirst = false;
+			for (String binding : bindings) {
+				if (notFirst)
+					writer.write(", ");
+				else
+					notFirst = true;
+				writer.write("\"" + StringEscapeUtils.escapeXml(binding) + "\": {");
+				
+				RDFNode node = soln.get(binding);
+				if (node.isLiteral()) {
+					Literal literal = (Literal) node;
+					writer.write("type: \"literal\", ");
+					if (literal.getDatatypeURI() != null)
+						writer.write("datatype: \"" + literal.getDatatypeURI() + "\", ");
+					if (literal.getLanguage() != null)
+						writer.write("datatype: \"" + StringEscapeUtils.escapeJavaScript(literal.getLanguage()) + "\", ");
+					writer.write("value: \"" + StringEscapeUtils.escapeJavaScript(literal.getLexicalForm()) + "\"");
+				} else if (node.isAnon()) {
+					writer.write("type: \"bnode\", ");
+					writer.write("label: \"" + StringEscapeUtils.escapeJavaScript(VelocityResource.create((Resource) node, null).getLabel()) + "\", ");
+					writer.write("id: \"" + ((Resource) node).getId() + "\"");
+				} else if (node.isURIResource()) {
+					writer.write("type: \"uri\", ");
+					writer.write("label: \"" + StringEscapeUtils.escapeJavaScript(VelocityResource.create((Resource) node, null).getLabel()) + "\", ");
+					writer.write("uri: \"" + StringEscapeUtils.escapeJavaScript(((Resource) node).getURI()) + "\"");
+				}
+				writer.write("}");
+			}
+			writer.write("}");
+			if (resultset.hasNext())
+				writer.write(", ");
+		}
+		writer.write("}}");
+		if (asJavaScript && req.getParameter("callback") != null)
+			writer.write(");");
+		writer.write("\n");
+		
 	}
 	
 	private String _(String s) {
